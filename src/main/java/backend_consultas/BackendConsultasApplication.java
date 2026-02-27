e13849501a9fabd5540d520d5894d04cc206daa8
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-27 08:38:25 -0300

    chore: estrutura inicial gerada pelo Spring Initializr

---
+++ src/main/java/backend_consultas/BackendConsultasApplication.java
@@ -0,0 +1,13 @@
+package backend_consultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendConsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendConsultasApplication.class, args);
+	}
+
+}
