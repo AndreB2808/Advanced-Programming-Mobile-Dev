f7ddd5840d1fdba21ab0db31fa8fa630c2530cf9
AndreB2808 <andre.bartolo.pds@gmail.com>
2026-03-02 09:01:56 -0300

    feat: criação do repository de Paciente

---
+++ src/main/java/backend_consultas/repository/PacienteRepository.java
@@ -0,0 +1,5 @@
+package backend_consultas.repository;
+import backend_consultas.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+}
