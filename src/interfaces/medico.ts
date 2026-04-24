3c277b3db2322ff04b510dd300fe5be1618c623d
AndreB2808 <andre.bartolo.pds@gmail.com>
2026-04-24 08:46:47 -0300

    Criação do Spring Boot base

--- src/interfaces/medico.ts
+++
@@ -1,8 +0,0 @@
-import { Especialidade } from "../types/especialidades";
-export interface Medico {
-  id: number;
-  nome: string;
-  crm: string;
-  especialidade: Especialidade;
-  ativo: boolean;
-}
