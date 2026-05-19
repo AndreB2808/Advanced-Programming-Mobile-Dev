66f5ad258479393e4a4db76da2d6ee6a2731331c
AndreB2808 <andre.bartolo.pds@gmail.com>
2026-05-19 20:59:56 -0300

    #

--- src/main/java/com/example/sprint1/repository/AlertaIndustrialRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sprint1.repository;
-
-import com.example.sprint1.model.AlertaIndustrial;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertaIndustrialRepository extends JpaRepository<AlertaIndustrial, Long> {
-}
